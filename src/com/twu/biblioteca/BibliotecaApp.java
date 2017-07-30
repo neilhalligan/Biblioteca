4280bf7e1c0c96184b254da55ddc24231fd34a9f
neil halligan <neilhalligan2972@gmail.com>
2017-07-30 13:10:39 +0100

    return book feature -unfinished

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.util.*;
 
 public class BibliotecaApp {
-    List<Book> bookList = new ArrayList<Book>();
+    List<Book> libraryBooks = new ArrayList<Book>();
     List<Book> customerBooks = new ArrayList<Book>();
     private boolean running = true;
 
@@ -17,10 +17,9 @@ public class BibliotecaApp {
     public void go() {
         welcome();
         populateBookList();
-        printMenu();
+        printMainMenu();
         while (running) {
-//            selectMenu(getInput(getReader()), getInput(getReader()));
-            selectMenu(getReader(), getReader());
+            selectMainMenu(getReader());
         }
     }
 
@@ -29,72 +28,82 @@ public class BibliotecaApp {
     }
 
     public void populateBookList(){
-        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1922);
-        Book book2 = new Book("Moby Dick","Herman Melville", 1851);
-        bookList.add(book1);
-        bookList.add(book2);
+        Book bookA = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1922);
+        Book bookB = new Book("Moby Dick","Herman Melville", 1851);
+        Book bookC = new Book("Frankenstein","Mary Shelley", 1818);
+        libraryBooks.add(bookA);
+        libraryBooks.add(bookB);
+        libraryBooks.add(bookC);
+        addToCustomerBooks(bookC);
     }
 
-    public void printMenu(){
-        System.out.println("Main Menu\n1 - List Books\n2 - Quit");
+    public void printMainMenu(){
+        System.out.println("Main Menu\n1 - List Books\n2 - Return Books\n3 - Quit");
     }
 
-    public String getInput(BufferedReader br){
-        String input = "";
-        try {
-            input = askUserInput(br);
-            System.out.println("getInput input var is: " + input);
-        } catch(IOException ex) {
-            System.out.println(ex.toString());
-        }
-        return input;
-    }
-
-    public void selectMenu(BufferedReader bufferSelector, BufferedReader bookSelector) {
-//    public void selectMenu(String menuSelector, String bookSelector) {
+    public void selectMainMenu(BufferedReader bufferSelector) {
         String menuSelector = getInput(bufferSelector);
-            System.out.println("menu selector is: " + menuSelector);
         if (menuSelector.equals("1")) {
             printBookList();
             selectBookToCheckout(getInput(bufferSelector));
-        } else if (menuSelector.equals("2")){
+        } else if (menuSelector.equals("2")) {
+            printReturnBookList();
+            selectBookToReturn(getInput(bufferSelector));
+        } else if (menuSelector.equals("3")){
             quit();
         } else{
             System.out.println("Select a valid menu option!");
         }
     }
 
-    public void selectBookToCheckout(String selector) {
-        System.out.println("bookselector is: " + selector);
-
-        Book bookToCheckout = null;
-
-        for (Book book : bookList) {
-            if (selector.equals(book.getTitle())) {
-                bookToCheckout = book;
-            }
+    public void printBookList(){
+        System.out.println("Type book title to checkout book, type \"back\" to go back");
+        for(Book book : libraryBooks) {
+            System.out.println(book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished());
         }
+    }
 
+    public void printReturnBookList(){
+        System.out.println("Type book title to return book, type \"back\" to go back");
+        for(Book book : customerBooks) {
+            System.out.println(book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished());
+        }
+    }
+
+    public void selectBookToCheckout(String selector) {
+        Book bookToCheckout = findBookByTitle(selector);
         if (bookToCheckout != null) {
             addToCustomerBooks(bookToCheckout);
-            System.out.println(bookToCheckout.getTitle() + " successfully checked out!");
-        } else if (selector.equals("0\n")) {
-            printMenu();
+            System.out.println("Thank you! Enjoy the book");
+            printMainMenu();
+        } else if (selector.equals("back")) {
+            printMainMenu();
         } else {
-            System.out.println("Select a valid book!");
+            System.out.println("That book is not available.");
+        }
+    }
+
+    public void selectBookToReturn(String selector) {
+        Book bookToCheckout = findBookByTitle(selector);
+        if (bookToCheckout != null) {
+            returnToLibraryBooks(bookToCheckout);
+            System.out.println("Thank you! Enjoy the book");
+            printMainMenu();
+        } else if (selector.equals("back")) {
+            printMainMenu();
+        } else {
+            System.out.println("That book is not available.");
         }
     }
 
     public void addToCustomerBooks(Book book) {
         customerBooks.add(book);
-        bookList.remove(book);
+        libraryBooks.remove(book);
     }
 
-    public void printBookList(){
-        System.out.println("Type book number to checkout book, type \"back\" to go back");
-        for(Book book : bookList) {
-            System.out.println(book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished());
-        }
+    public void returnToLibraryBooks(Book book) {
+        customerBooks.remove(book);
+        libraryBooks.add(book);
     }
 
     private BufferedReader getReader() {
@@ -111,5 +120,25 @@ public class BibliotecaApp {
     private void quit() {
         System.out.println("Bye!");
         running = false;
+    }
+
+    private String getInput(BufferedReader br){
+        String input = "";
+        try {
+            input = askUserInput(br);
+        } catch(IOException ex) {
+            System.out.println(ex.toString());
+        }
+        return input;
+    }
+
+    private Book findBookByTitle(String bookTitle) {
+        Book correctBook = null;
+        for (Book book : libraryBooks) {
+            if (bookTitle.equals(book.getTitle())) {
+                correctBook = book;
+            }
+        }
+        return correctBook;
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -10,6 +10,10 @@ public class BibliotecaAppTest {
     private BibliotecaApp biblioteca;
     private Book book1;
     private Book book2;
+    private Book book3;
+    private String bookMenuMessage;
+    private String mainMenuMessage;
+    private String returnMenuMessage;
 
     @Before
     public void setUp() throws Exception {
@@ -19,6 +23,10 @@ public class BibliotecaAppTest {
         biblioteca.populateBookList();
         book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1922);
         book2 = new Book("Moby Dick","Herman Melville", 1851);
+        book3 = new Book("Frankenstein","Mary Shelley", 1818);
+        bookMenuMessage = "Type book title to checkout book, type \"back\" to go back\n";
+        mainMenuMessage = "Main Menu\n1 - List Books\n2 - Return Books\n3 - Quit\n";
+        returnMenuMessage = "Type book title to return book, type \"back\" to go back\n";
     }
 
     @Test
@@ -31,109 +39,167 @@ public class BibliotecaAppTest {
     @Test
     public void hasBookList() throws Exception {
         List<Book> mockBookList = new ArrayList<Book>(Arrays.asList(book1, book2));
-        assertEquals(mockBookList, biblioteca.bookList);
+        assertEquals(mockBookList, biblioteca.libraryBooks);
     }
 
     @Test
     public void populateBookList() throws Exception {
-        assertEquals(book1, biblioteca.bookList.get(0));
-        assertEquals(book2, biblioteca.bookList.get(1));
+        assertEquals(book1, biblioteca.libraryBooks.get(0));
+        assertEquals(book2, biblioteca.libraryBooks.get(1));
     }
 
-//    @Test
-//    public void printBookList() throws Exception {
-//        biblioteca.printBookList();
-//        String expectedOutput = "Type book title name to checkout book\n";
-//        expectedOutput += book1.getTitle() + " | " + book1.getAuthor() + " | " + book1.getYearPublished() + "\n";
-//        expectedOutput += book2.getTitle() + " | " + book2.getAuthor() + " | " + book2.getYearPublished() + "\n";
-//        assertEquals(expectedOutput, outContent.toString());
-//    }
-
     @Test
-    public void printMenu() throws Exception {
-        biblioteca.printMenu();
-        String expectedOutput  = "Main Menu\n1 - List Books\n2 - Quit\n";
+    public void printMainMenu() throws Exception {
+        biblioteca.printMainMenu();
+        String expectedOutput  = mainMenuMessage;
         assertEquals(expectedOutput, outContent.toString());
     }
 
-//    @Test
-//    public void mainMenuSelect() throws Exception {
-//        // could refactor by adding Stream
-//        String expectedOutput = "Select a valid menu option!\n";
-//        biblioteca.selectMenu("r", "r");
-//
-//        // below test is tested in select book to checkout. but must assert that book list is printed correctly!!
-//
-////        expectedOutput += "Type book number to checkout book, type \"back\" to go back\n";
-////        for(Book book : biblioteca.bookList) {
-////            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
-////        }
-////        biblioteca.selectMenu("1", "r");
-//
-//        expectedOutput += "Bye!\n";
-//        biblioteca.selectMenu("2", "r");
-//        assertEquals(expectedOutput, outContent.toString());
-//    }
-
     @Test
-    public void selectBookToCheckout() throws Exception {
-        Book firstBook = biblioteca.bookList.get(0);
-        Book secondBook = biblioteca.bookList.get(1);
+    public void printBookList() throws Exception {
+        String expectedOutput = bookMenuMessage;
+        for(Book book : biblioteca.libraryBooks) {
+            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
+        }
 
-        biblioteca.selectBookToCheckout(firstBook.getTitle());
-        biblioteca.selectBookToCheckout(secondBook.getTitle());
-//        Book firstBook = biblioteca.bookList.get(0);
-//        Book firstBook = biblioteca.bookList.get(Integer.parseInt("1") - 1);
-        String expectedOutput  = firstBook.getTitle() + " successfully checked out!\n";
-
-//        biblioteca.selectBookToCheckout("1");
-//        Book secondBook = biblioteca.bookList.get(0);
-        expectedOutput  += secondBook.getTitle() + " successfully checked out!\n";
-
-//        assertEquals(biblioteca.customerBooks.get(0), firstBook);
-//        assertEquals(biblioteca.customerBooks.get(1), secondBook);
-//        assertFalse(biblioteca.bookList.contains(firstBook));
-//        assertFalse(biblioteca.bookList.contains(secondBook));
+        biblioteca.printBookList();
         assertEquals(expectedOutput, outContent.toString());
     }
 
+    @Test
+    public void mainMenuInvalidSelection() throws Exception {
+        Reader InputString = new StringReader("invalid\n");
+        BufferedReader menuInput = new BufferedReader(InputString);
+        String expectedOutput = "Select a valid menu option!\n";
+        biblioteca.selectMainMenu(menuInput);
+        assertEquals(expectedOutput, outContent.toString());
+    }
 
-//    @Test
-//    public void name() throws Exception {
-//        Reader menuInputString = new StringReader("1\n");
-//        BufferedReader menuInput = new BufferedReader(menuInputString);
-//
-//        Reader bookInputString = new StringReader("The Great Gatsby\n");
-//        BufferedReader bookInput = new BufferedReader(bookInputString);
-//
-//        String expectedOutput = "Type book number to checkout book, type \"back\" to go back\n";
-//        for(Book book : biblioteca.bookList) {
-//            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
-//        }
-//
-//        biblioteca.selectMenu(biblioteca.getInput(menuInput), biblioteca.getInput(bookInput));
-//        expectedOutput += "The Great Gatsby successfully checked out!\n";
-//        assertEquals(expectedOutput, outContent.toString());
-//    }
+    @Test
+    public void mainMenuSelectReturnBooks() throws Exception {
+        Reader InputString = new StringReader("2\n");
+        BufferedReader menuInput = new BufferedReader(InputString);
+        String expectedOutput = "Type book title to return book, type \"back\" to go back\n";
+        for(Book book : biblioteca.customerBooks) {
+            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
+        }
+        biblioteca.selectMainMenu(menuInput);
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    public void mainMenuQuit() throws Exception {
+        Reader InputString = new StringReader("3\n");
+        BufferedReader menuInput = new BufferedReader(InputString);
+        String expectedOutput = "Bye!\n";
+        biblioteca.selectMainMenu(menuInput);
+        assertEquals(expectedOutput, outContent.toString());
+    }
 
     @Test
     public void hasCustomerBooks() throws Exception {
         List<Book> testCustomerBooks = new ArrayList<Book>();
+        testCustomerBooks.add(book3);
         assertEquals(biblioteca.customerBooks, testCustomerBooks);
     }
 
     @Test
     public void addToCustomerBooks() throws Exception {
-        Book book = biblioteca.bookList.get(0);
-        biblioteca.addToCustomerBooks(book);
-        assertEquals(biblioteca.customerBooks.get(0), book1);
-        assertFalse(biblioteca.bookList.contains(book1));
+        Book customerBook = biblioteca.libraryBooks.get(0);
+        biblioteca.addToCustomerBooks(customerBook);
+        assertTrue(biblioteca.customerBooks.contains(customerBook));
+        assertFalse(biblioteca.libraryBooks.contains(customerBook));
     }
 
     @Test
-    public void selectBookToCheckoutByTitle() throws Exception {
-        biblioteca.selectBookToCheckout("The Great Gatsby");
-        String expectedOutput = "The Great Gatsby successfully checked out!\n";
+    public void returnToLibraryBooks() throws Exception {
+        Book returnBook = biblioteca.customerBooks.get(0);
+        biblioteca.returnToLibraryBooks(returnBook);
+        assertTrue(biblioteca.libraryBooks.contains(returnBook));
+        assertFalse(biblioteca.customerBooks.contains(returnBook));
+    }
+
+    @Test
+    public void checkoutBookAndPrintMainMenu() throws Exception {
+        Book bookToCheckout = biblioteca.libraryBooks.get(0);
+        Reader menuInputString = new StringReader("1\n" + bookToCheckout.getTitle());
+        BufferedReader menuInput = new BufferedReader(menuInputString);
+        String expectedOutput = bookMenuMessage;
+        for(Book book : biblioteca.libraryBooks) {
+            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
+        }
+        biblioteca.selectMainMenu(menuInput);
+        expectedOutput += "Thank you! Enjoy the book\n";
+        expectedOutput += mainMenuMessage;
         assertEquals(expectedOutput, outContent.toString());
     }
+
+    @Test
+    public void checkoutMenuGoBack() throws Exception {
+        Reader menuInputString = new StringReader("1\nback\n");
+        BufferedReader menuInput = new BufferedReader(menuInputString);
+        String expectedOutput = bookMenuMessage;
+        for(Book book : biblioteca.libraryBooks) {
+            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
+        }
+        biblioteca.selectMainMenu(menuInput);
+        expectedOutput += mainMenuMessage;
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    public void checkoutMenuInvalidSelection() throws Exception {
+        Reader menuInputString = new StringReader("1\ninvalid selection\n");
+        BufferedReader menuInput = new BufferedReader(menuInputString);
+        String expectedOutput = bookMenuMessage;
+        for(Book book : biblioteca.libraryBooks) {
+            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
+        }
+        biblioteca.selectMainMenu(menuInput);
+        expectedOutput += "That book is not available.\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    public void returnBookAndPrintMainMenu() throws Exception {
+        Book bookToReturn = biblioteca.customerBooks.get(0);
+        Reader menuInputString = new StringReader("2\n" + bookToReturn.getTitle());
+        BufferedReader menuInput = new BufferedReader(menuInputString);
+        String expectedOutput = returnMenuMessage;
+        for(Book book : biblioteca.customerBooks) {
+            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
+        }
+        biblioteca.selectMainMenu(menuInput);
+        expectedOutput += "Thank you for returning the book.\n";
+        expectedOutput += mainMenuMessage;
+        assertTrue(biblioteca.libraryBooks.contains(bookToReturn));
+        assertFalse(biblioteca.customerBooks.contains(bookToReturn));
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+//    @Test
+//    public void returnMenuGoBack() throws Exception {
+//        Reader menuInputString = new StringReader("1\nback\n");
+//        BufferedReader menuInput = new BufferedReader(menuInputString);
+//        String expectedOutput = bookMenuMessage;
+//        for(Book book : biblioteca.libraryBooks) {
+//            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
+//        }
+//        biblioteca.selectMainMenu(menuInput);
+//        expectedOutput += mainMenuMessage;
+//        assertEquals(expectedOutput, outContent.toString());
+//    }
+//
+//    @Test
+//    public void returnMenuInvalidSelection() throws Exception {
+//        Reader menuInputString = new StringReader("1\ninvalid selection\n");
+//        BufferedReader menuInput = new BufferedReader(menuInputString);
+//        String expectedOutput = bookMenuMessage;
+//        for(Book book : biblioteca.libraryBooks) {
+//            expectedOutput += book.getTitle() + " | " + book.getAuthor() + " | " + book.getYearPublished() + "\n";
+//        }
+//        biblioteca.selectMainMenu(menuInput);
+//        expectedOutput += "That book is not available.\n";
+//        assertEquals(expectedOutput, outContent.toString());
+//    }
 }
